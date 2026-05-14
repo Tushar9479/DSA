d68b08d454df3053e297204abc7048b95118e23d
Tushar Yadav <tusharyadav09092005@gmail.com>
2026-05-14 15:11:14 +0530

    sortarraybuiltin

--- src/Arrays/SortArrayBuiltIn.java
+++ src/Arrays/SortArrayBuiltIn.java
@@ -1,7 +1,20 @@
 package Arrays;
 
+import java.util.Arrays;
+
 public class SortArrayBuiltIn {
     static void main(String[] args) {
+        int [] arr = {12,34,5,2,4,-69};
+        print(arr);
+        Arrays.sort(arr);
+        print(arr);
 
+
+    }
+    public static void print(int []arr){
+        for (int i = 0; i < arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
     }
 }
