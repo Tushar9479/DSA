03a52159b1b10242148051dd88566d7880a99215
Tushar Yadav <tusharyadav09092005@gmail.com>
2026-05-16 13:32:42 +0530

    forEach

---
+++ src/Arrays/ForEach.java
@@ -0,0 +1,10 @@
+package Arrays;
+
+public class ForEach {
+    static void main(String[] args) {
+        int [] arr = {1,2,3,5,46,7};
+        for (int ele: arr){
+            System.out.println(ele+" ");
+        }
+    }
+}
