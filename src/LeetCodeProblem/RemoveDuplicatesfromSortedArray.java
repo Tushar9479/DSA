3324669a96d1dc34ffafe3ddaa3abf7dd9a4e756
Tushar Yadav <tusharyadav09092005@gmail.com>
2026-05-28 14:37:15 +0530

    RemoveDuplicatesfromSortedArray

---
+++ src/LeetCodeProblem/RemoveDuplicatesfromSortedArray.java
@@ -0,0 +1,18 @@
+package LeetCodeProblem;
+//only logic
+
+public class RemoveDuplicatesfromSortedArray {
+    class Solution {
+        public int removeDuplicates(int[] nums) {
+            int k = 0;
+            for(int i = 0; i < nums.length; i++){
+                if(nums[i]!=nums[k]){
+                    k++;
+                    nums[k]=nums[i];
+                }
+            }
+            return k+1;
+        }
+    }
+
+}
