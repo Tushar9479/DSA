2b524cc91ce3d760b545b23e8d182c66306a6696
Tushar Yadav <tusharyadav09092005@gmail.com>
2026-06-09 15:19:41 +0530

    Count Digit

---
+++ src/Strings/CountDigits.java
@@ -0,0 +1,20 @@
+package Strings;
+
+import java.util.Scanner;
+
+public class CountDigits {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String s = "" + n;
+        System.out.println(s.length());
+
+
+//        int count  = 0;
+//        while(n!=0){
+//            n /= 10;
+//            count++;
+//        }
+//        System.out.println(count);
+    }
+}
