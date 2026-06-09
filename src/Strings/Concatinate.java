4061a39c578378a3a17bf7d670fbf0a7c0e8ac3d
Tushar Yadav <tusharyadav09092005@gmail.com>
2026-06-09 15:20:47 +0530

    Concatinate

---
+++ src/Strings/Concatinate.java
@@ -0,0 +1,13 @@
+package Strings;
+
+public class Concatinate {
+    static void main(String[] args) {
+        String a = "shivam";
+        String b = "yadav";
+
+//        a.concat(b); // It is not working but if do it like that
+//        a = a.concat(b); it work
+//        System.out.println(a);
+      System.out.println(a.concat(b));
+    }
+}
