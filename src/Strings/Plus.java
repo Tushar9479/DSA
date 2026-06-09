17bac6cd9d1ea65a6e63d2ce38d912df5d56fbd7
Tushar Yadav <tusharyadav09092005@gmail.com>
2026-06-09 15:21:55 +0530

    Plus

---
+++ src/Strings/Plus.java
@@ -0,0 +1,19 @@
+package Strings;
+
+public class Plus {
+    static void main(String[] args) {
+        String s = "tushar";
+        s = s + "is king of the world";
+        s += 10;
+        s += "\n";
+        s += 'a';
+        System.out.println(s);
+        System.out.println("shivam"+9+20); // string + integer = string
+        System.out.println("shivam"+(9+20)); // string + integer = string
+        System.out.println(10+"shivam"+20); // string + integer = string
+        System.out.println(10+20+"shivam"); // string + integer = string
+        System.out.println('A'+"shivam"); // string + integer = string
+        System.out.println('A'+2+"shivam"); // string + integer = string
+
+    }
+}
