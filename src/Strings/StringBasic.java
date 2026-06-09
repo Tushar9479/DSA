5cdac9c7bfca3f89b1f45e62d35f65b1ec01db2f
Tushar Yadav <tusharyadav09092005@gmail.com>
2026-06-09 15:22:06 +0530

    String Basic

---
+++ src/Strings/StringBasic.java
@@ -0,0 +1,13 @@
+package Strings;
+
+import java.util.Scanner;
+
+public class StringBasic {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String x = sc.nextLine();
+        System.out.println(x);
+        String t = sc.next();
+        System.out.println(t);
+    }
+}
