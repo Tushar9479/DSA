c62befb0e4096140ce8dc2567009fb3a6a9646c4
Tushar Yadav <tusharyadav09092005@gmail.com>
2026-06-10 14:33:15 +0530

    String To Char Array

---
+++ src/Strings/StringToCharArray.java
@@ -0,0 +1,14 @@
+package Strings;
+
+public class StringToCharArray {
+    static void main(String[] args) {
+        String s = "Tushar";
+        char[] arr = s.toCharArray();
+
+        for(char ch : arr){
+            System.out.print(ch);
+        }
+        //isTareekKoHamCamelCaseKehteHai
+        //IsWayKoPascalCaseKheteHain
+    }
+}
