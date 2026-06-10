c5872ee0bc3dba148963910043b2ae5079cdef63
Tushar Yadav <tusharyadav09092005@gmail.com>
2026-06-10 14:33:33 +0530

    Sub Strings Of String

---
+++ src/Strings/SubStringsOfString.java
@@ -0,0 +1,16 @@
+package Strings;
+
+public class SubStringsOfString {
+    static void main(String[] args) {
+//        String s = "tushar";
+//        System.out.println(s);
+//        s = "shivam";
+//        s.charAt(0) = 'm'; // not possible
+//        System.out.println(s);
+
+        String s = "Jaishanker";
+        System.out.println(s.substring(3));
+        System.out.println(s.substring(1,5));
+
+    }
+}
