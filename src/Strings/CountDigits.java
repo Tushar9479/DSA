7219893c9e763dd600b4f1fdc76951c50b4eaea6
Tushar Yadav <tusharyadav09092005@gmail.com>
2026-06-10 14:31:36 +0530

    Count Digits

--- src/Strings/CountDigits.java
+++ src/Strings/CountDigits.java
@@ -4,10 +4,16 @@ import java.util.Scanner;
 
 public class CountDigits {
     static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        String s = "" + n;
-        System.out.println(s.length());
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();
+//        String s = "" + n;
+//        System.out.println(s.length());
+
+
+        Scanner sd = new Scanner(System.in);
+        double d  = sd.nextDouble();
+        String s1 = "" + d;
+        System.out.println(s1.length());
 
 
 //        int count  = 0;
