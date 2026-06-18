e4eda489892edd7217f6d7c14cdb5ac4637e4393
Tushar Yadav <tusharyadav09092005@gmail.com>
2026-06-18 10:20:09 +0530

    Reverse Only Letters

---
+++ src/LeetCodeProblem/ReverseOnlyLetters.java
@@ -0,0 +1,27 @@
+package LeetCodeProblem;
+
+public class ReverseOnlyLetters {
+    class Solution {
+        public String reverseOnlyLetters(String s) {
+            int i = 0;
+            int j = s.length()-1;
+            char ch[] = s.toCharArray();
+            while(i<j){
+                if(!Character.isLetter(ch[i])){
+                    i++;
+                }
+                else if(!Character.isLetter(ch[j])){
+                    j--;
+                }
+                else{
+                    char temp = ch[i];
+                    ch[i] = ch[j];
+                    ch[j] = temp;
+                    i++;
+                    j--;
+                }
+            }
+            return new String(ch);
+        }
+    }
+}
