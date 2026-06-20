091bdad6a6450f3383119646f6210129f6bcab80
Tushar Yadav <tusharyadav09092005@gmail.com>
2026-06-20 11:11:07 +0530

    Find Peak Element

--- src/LeetCodeProblem/FindPeakElement.java
+++ src/LeetCodeProblem/FindPeakElement.java
@@ -16,4 +16,19 @@ public class FindPeakElement {
             return 0;
         }
     }
+
+//    int start = 0;
+//    int end = nums.length - 1;
+//
+//        while (start < end) {
+//        int mid = start + (end - start) / 2;
+//
+//        if (nums[mid] > nums[mid + 1]) {
+//            end = mid;
+//        } else {
+//            start = mid + 1;
+//        }
+//    }
+//
+//        return start;
 }
