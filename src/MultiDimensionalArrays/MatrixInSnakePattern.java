5dba60480777b83d439b8bd5283832ec5ac39eb6
Tushar Yadav <tusharyadav09092005@gmail.com>
2026-06-21 15:38:56 +0530

    Matrix In Snake Pattern

---
+++ src/MultiDimensionalArrays/MatrixInSnakePattern.java
@@ -0,0 +1,21 @@
+package MultiDimensionalArrays;
+
+public class MatrixInSnakePattern {
+    static void main(String[] args) {
+        int [][] arr = {{3,4,5,6},{1,4,6,8},{5,7,3,7}};
+        for(int i =0;i<arr.length;i++){
+            if(i%2 == 0){
+                for (int j =0;j<arr[0].length;j++){
+                    System.out.print(arr[i][j]);
+                }
+            }
+            else{
+                for (int j =arr[0].length -1 ;j>=0;j--){
+                    System.out.print(arr[i][j]);
+                }
+            }
+            System.out.println();
+        }
+    }
+
+}
