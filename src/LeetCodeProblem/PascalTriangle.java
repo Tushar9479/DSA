9113b4de93ef9d2e1acc30b8ef16211fb283c119
Tushar Yadav <tusharyadav09092005@gmail.com>
2026-06-23 09:39:41 +0530

    Pascals Triangle

---
+++ src/LeetCodeProblem/PascalTriangle.java
@@ -0,0 +1,46 @@
+package LeetCodeProblem;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PascalTriangle {
+    class Solution {
+        public List<List<Integer>> generate(int n) {
+            List<List<Integer>> ans = new ArrayList<>();
+            for(int i = 0;i < n; i++){
+                ans.add(new ArrayList<>());
+                for (int j = 0;j<=i; j++){
+                    if(j == 0 || j == i) ans.get(i).add(1);
+                    else {
+                        int val = ans.get(i-1).get(j)+ans.get(i-1).get(j-1);
+                        ans.get(i).add(val);
+                    }
+                }
+            }
+            return ans;
+        }
+    }
+    //***************************M2*****************************
+    class Solution {
+        public List<List<Integer>> generate(int n) {
+            List<List<Integer>> ans = new ArrayList<>();
+            for(int i = 0;i < n; i++){
+                List<Integer> list = new ArrayList<>();
+                for(int j=0;j<=i;j++){
+                    list.add(1);
+                }
+                ans.add(list);
+            }
+            for(int i = 0;i < n; i++){
+                for (int j = 0;j<=i; j++){
+                    if(j == 0 || j == i) ans.get(i).set(j,1);
+                    else {
+                        int val = ans.get(i-1).get(j)+ans.get(i-1).get(j-1);
+                        ans.get(i).set(j,val);
+                    }
+                }
+            }
+            return ans;
+        }
+    }
+}
