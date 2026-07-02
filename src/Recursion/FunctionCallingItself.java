a631e622f10bfef6319ffa04da393038fbb722fc
Tushar Yadav <tusharyadav09092005@gmail.com>
2026-07-02 10:08:10 +0530

    Function Calling Itself

---
+++ src/Recursion/FunctionCallingItself.java
@@ -0,0 +1,16 @@
+package Recursion;
+
+import java.util.Scanner;
+
+public class FunctionCallingItself {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        print(n);
+    }
+    public static void print(int n){
+        if(n==0) return;
+        System.out.println(n);
+        print(n-1);
+    }
+}
