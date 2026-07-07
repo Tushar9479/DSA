b740ceb3f43ece84d0025a8c5bdfc39323de71b3
Tushar Yadav <tusharyadav09092005@gmail.com>
2026-07-07 10:39:46 +0530

    Reverse Pairs

---
+++ src/LeetCodeProblem/ReversePairs.java
@@ -0,0 +1,50 @@
+package LeetCodeProblem;
+
+public class ReversePairs {
+    class Solution {
+        static int count;
+        public int reversePairs(int[] arr) {
+            count = 0;
+            mergeSort(arr);
+            return count;
+        }
+
+        private static void mergeSort(int[] arr) {
+            int n = arr.length;
+            if(n==1) return;
+            int [] a = new int[n/2];
+            int [] b = new int[n-n/2];
+            int idx = 0;
+            for(int i=0;i<a.length;i++)
+                a[i] = arr[idx++];
+            for(int i=0;i<b.length;i++)
+                b[i] = arr[idx++];
+
+            mergeSort(a);
+            mergeSort(b);
+
+            merge(a,b,arr);
+        }
+
+        public static void merge(int[] a, int[] b, int[] c){
+            int j=0;
+            for(int i=0;i<a.length;i++){
+                while(j<b.length && (long)a[i] > 2L*b[j]){
+                    j++;
+                }
+                count += j;
+            }
+
+            int i =0,k=0;
+            j=0;
+            while (i<a.length && j<b.length){
+                if(a[i]<=b[j]) c[k++] = a[i++];
+                else{
+                    c[k++] = b[j++];
+                }
+            }
+            while (i<a.length) c[k++] = a[i++];
+            while (j<b.length) c[k++] = b[j++];
+        }
+    }
+}
